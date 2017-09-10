b5d46e1102c501e19a8dd7616cddee9f2323c437
Ryan Kaplan <ryan_kaplan@me.com>
2017-09-09 20:57:51 -0700

    Removes commented out code and features that are not currently functional.

--- main.js
+++ main.js
@@ -3,43 +3,19 @@
 var $buttonStart = document.querySelector('#start')
 var $seconds = document.querySelector('#seconds')
 var $minutes = document.querySelector('#minutes')
-var time = 0
+var seconds = 0
+var minutes = 0
 
 function counter () {
-  time = time + 1
-  $seconds.textContent = time
+  seconds = seconds + 1
+  $seconds.textContent = seconds
+  $minutes.textContent = minutes
+  if (seconds >= 59) {
+    seconds = 0
+    minutes++
+  }
 }
 
 $buttonStart.addEventListener('click', function () {
   setInterval(function () { counter() }, 1000)
 })
-
-// Miscellaneous test code - Disregard
-
-/* function count() {
-  var i = 0
-  for (i = 0; i <= 60; i++) {
-    counter()
-    setInterval(counter(), 1000)
-  }
-} */
-
-/* function timer () {
-  seconds++
-  if (seconds >= 60) {
-    seconds = 0
-    minutes++
-  }
-} */
-
-/* function timer () {
-  setTimeout(timer, 1000)
-  for (s = 0; s <= 0; s++) {
-    seconds++
-    console.log(seconds)
-    if (s => 60) {
-      minutes = 0
-    }
-  }
-}
-*/
