9eb5bdee4391c28509f2eecb78d0d9cf3e9f7b93
Ryan Kaplan <ryan_kaplan@me.com>
2017-09-11 14:24:50 -0700

    Adds the abliity for the user to reset the stopwatch back to zero.

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 var $buttonStart = document.querySelector('#start')
 var $buttonStop = document.querySelector('#stop')
+var $buttonReset = document.querySelector('#reset')
 var $seconds = document.querySelector('#seconds')
 var $minutes = document.querySelector('#minutes')
 var seconds = 0
@@ -11,14 +12,27 @@ var timerId = null
 function counter () {
   seconds = seconds + 1
   $seconds.textContent = seconds
+  $minutes.textContent = minutes
   if (seconds >= 59) {
     seconds = 0
     minutes++
+  }
+  if (seconds < 10) {
+    $seconds.textContent = '0' + seconds
+  }
+  if (minutes < 10) {
+    $minutes.textContent = '0' + minutes
   }
 }
 
 function stop () {
   clearInterval(timerId)
+}
+
+function reset () {
+  $seconds.textContent = '00'
+  $minutes.textContent = '00'
+  seconds = 0
 }
 
 $buttonStart.addEventListener('click', function () {
@@ -27,4 +41,8 @@ $buttonStart.addEventListener('click', function () {
 
 $buttonStop.addEventListener('click', function () {
   stop()
+})
+
+$buttonReset.addEventListener('click', function () {
+  reset()
 })
